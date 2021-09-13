e3c7d20fee560fbbd04082ce31d903fd5ab48d17
Uriell <me+github@uriell.dev>
2021-09-13 11:26:20 -0300

    creating initial interfaces for basic concepts

--- internal/core/domain/domain.go
+++ internal/core/domain/domain.go
@@ -1,5 +1,29 @@
 package domain
 
 type File struct {
+	ProjectID string
+	FilePath  string
+}
 
+type Phrase struct {
+	FileID string
+	Key    string
+}
+
+type Suggestion struct {
+	PhraseID string
+	UserID   string
+	Language string
+	Approved bool
+}
+
+type Direction int
+
+const UPVOTE Direction = 1
+const DOWNVOTE Direction = -1
+
+type Vote struct {
+	SuggestionID string
+	UserID       string
+	Direction    Direction
 }
