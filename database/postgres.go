096ad33e0f74eaa14bdc640560403ff3b15c1aca
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-09-20 11:51:12 -0300

    create db-postgres connection

---
+++ database/postgres.go
@@ -0,0 +1,44 @@
+package database
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/ditointernet/tradulab-service/drivers"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Database struct {
+	db *gorm.DB
+}
+
+func MustNewDB() Database {
+	return Database{}
+}
+
+func (d *Database) StartPostgres() {
+	dns := "host=localhost port=25430 user=admin dbname=books sslmode=disable password=123456"
+
+	database, err := gorm.Open(postgres.Open(dns), &gorm.Config{})
+	if err != nil {
+		fmt.Println("Could not connect to the Postgres Database")
+		log.Fatal("error: ", err)
+	}
+
+	d.db = database
+
+}
+
+func (d *Database) GetDatabase() *gorm.DB {
+	return d.db
+}
+
+func (d *Database) SaveFile(file *drivers.File) error {
+	db := d.GetDatabase()
+
+	return db.Raw("INSERT into files (ID, ProjectID, FilePath) values (?,?,?)", file.ID, file.ProjectID, file.FilePath).Error
+
+	//stmt.Exec(file.ID, file.ProjectID, file.ProjectID)
+
+}
