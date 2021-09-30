ac030d53daa46265c8f933630d38cf6a20ae2857
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-09-30 12:52:25 -0300

    exclude ports package

--- internal/core/ports/services.go
+++
@@ -1,8 +0,0 @@
-package ports
-
-import "github.com/ditointernet/tradulab-service/internal/core/domain"
-
-type FileHandler interface {
-	CheckFile(*domain.File) error
-	SaveFile(*domain.File) error
-}
