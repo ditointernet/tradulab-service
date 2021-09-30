22724184ab52f21e445d5598108617b004f5f689
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-09-30 12:55:29 -0300

    refactoring file service

--- internal/core/services/file.go
+++ internal/core/services/file.go
@@ -5,34 +5,37 @@ import (
 	"path/filepath"
 
 	"github.com/ditointernet/tradulab-service/internal/core/domain"
-	"github.com/ditointernet/tradulab-service/internal/core/ports"
+	"github.com/ditointernet/tradulab-service/internal/repository"
 	"github.com/google/uuid"
 )
 
 type File struct {
-	repo ports.FileRepository
+	repo repository.FileRepository
 }
 
-func MustNewFile(repo ports.FileRepository) *File {
+func MustNewFile(repo repository.FileRepository) *File {
 	return &File{repo: repo}
 }
 
-func (f File) CheckFile(entry *domain.File) error {
+// func (f File) CheckFile(entry *domain.File) error {
 
+// 	extension := filepath.Ext(entry.FilePath)
+// 	if extension != ".csv" {
+// 		return errors.New("file not supported. Must be .csv")
+// 	}
+
+// 	return nil
+// }
+
+func (f *File) SaveFile(entry *domain.File) error {
 	extension := filepath.Ext(entry.FilePath)
 	if extension != ".csv" {
 		return errors.New("file not supported. Must be .csv")
 	}
 
-	return nil
-}
-
-func (f *File) SaveFile(entry *domain.File) error {
-
 	entry.ID = uuid.New().String()
 
 	err := f.repo.SaveFile(entry)
-
 	if err != nil {
 		return err
 	}
