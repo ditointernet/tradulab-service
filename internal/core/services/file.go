5b58749d5842ef6f6b3710edc4bdb5858209f04e
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-09-30 14:12:34 -0300

    moving the CheckFile function call to the service

--- internal/core/services/file.go
+++ internal/core/services/file.go
@@ -17,25 +17,26 @@ func MustNewFile(repo repository.FileRepository) *File {
 	return &File{repo: repo}
 }
 
-// func (f File) CheckFile(entry *domain.File) error {
+func (f File) CheckFile(entry *domain.File) error {
 
-// 	extension := filepath.Ext(entry.FilePath)
-// 	if extension != ".csv" {
-// 		return errors.New("file not supported. Must be .csv")
-// 	}
-
-// 	return nil
-// }
-
-func (f *File) SaveFile(entry *domain.File) error {
 	extension := filepath.Ext(entry.FilePath)
 	if extension != ".csv" {
 		return errors.New("file not supported. Must be .csv")
 	}
 
+	return nil
+}
+
+func (f *File) SaveFile(entry *domain.File) error {
+	err := f.CheckFile(entry)
+
+	if err != nil {
+		return err
+	}
+
 	entry.ID = uuid.New().String()
 
-	err := f.repo.SaveFile(entry)
+	err = f.repo.SaveFile(entry)
 	if err != nil {
 		return err
 	}
